415cc7661ce491e7da2a4666b782109a05625b2c
Hytone <hannu_94@hotmail.com>
2022-08-08 20:20:07 +0300

    Add project files.

---
+++ Hytone.Timberborn.Mods.CustomTopBar/CustomTopBarPlugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using System;
+using TimberbornAPI;
+using TimberbornAPI.Common;
+
+namespace Hytone.Timberborn.Mods.CustomTopBar
+{
+    [BepInPlugin(PluginId, PluginName, PluginVersion)]
+    [BepInDependency("com.timberapi.timberapi")]
+    public class CustomTopBarPlugin : BaseUnityPlugin
+    {
+        public const string PluginId = "hytone.plugins.customtopbar";
+        public const string PluginName = "FoodMaterialsTopBar";
+        public const string PluginVersion = "0.0.1";
+
+        public void Awake()
+        {
+            TimberAPI.AssetRegistry.AddSceneAssets(PluginName, SceneEntryPoint.Global);
+            Logger.LogInfo($"Loaded {PluginName}.");
+        }
+
+    }
+}
